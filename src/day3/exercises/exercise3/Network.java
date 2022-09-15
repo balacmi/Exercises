6a3959a0144e21d676e73ff8da122809b6d32512
Milos Balac <milos.balac@ivt.baug.ethz.ch>
2022-09-15 10:26:02 +0200

    solutions for exercises from Day 3

---
+++ src/day3/exercises/exercise3/Network.java
@@ -0,0 +1,20 @@
+package day3.exercises.exercise3;
+
+public class Network {
+
+	private Node[] nodes;	
+	private Link[] links;
+	
+	public Network(Node[] nodes, Link[] links) {
+		
+		this.nodes = nodes;
+		this.links = links;
+	}
+	
+	public Node[] getNodes() {
+		return nodes;
+	}
+	public Link[] getLinks() {
+		return links;
+	}
+}
---
+++ src/day3/exercises/exercise4/PlanElement.java
@@ -0,0 +1,5 @@
+package day3.exercises.exercise4;
+
+public interface PlanElement {
+
+}
